db811808d3190e934306d3128a262acc94963335
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-11-05 16:03:48 +0300

    .

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import L from "leaflet";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, useMap, Marker } from "react-leaflet";
 
 import "leaflet/dist/leaflet.css";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
